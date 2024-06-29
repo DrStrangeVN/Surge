bce6a34958960559c2cef9759f1aeae19e09f0ec
DR STRANGE <160754110+DrStrangeVN@users.noreply.github.com>
2024-06-29 21:47:50 +0700

    Update Locket.js

--- Locket.js
+++ Locket.js
@@ -1,4 +1,3 @@
-Translate to English   
 var request = $request;
 
 const options = {
@@ -16,17 +15,17 @@ const ent = JSON.parse(data);
 
 let jsonToUpdate = {
         "request_date_ms": 1704070861000,
-        "request_date": "2006-03-04T01:01:01Z",
+        "request_date": "2006-04-03T01:01:01Z",
         "subscriber": {
             "entitlement": {},
-            "first_seen": "2006-03-04T01:01:01Z",
+            "first_seen": "2006-04-03T01:01:01Z",
             "original_application_version": "9692",
-            "last_seen": "2006-03-04T01:01:01Z",
+            "last_seen": "2006-04-03T01:01:01Z",
             "other_purchases": {},
             "management_url": null,
             "subscriptions": {},
             "entitlements": {},
-            "original_purchase_date": "2006-03-04T01:01:01Z",
+            "original_purchase_date": "2006-04-03T01:01:01Z",
             "original_app_user_id": "70B24288-83C4-4035-B001-573285B21AE2",
             "non_subscriptions": {}
         }
@@ -41,8 +40,8 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
 
   for (const entitlement of entitlements) {
     jsonToUpdate.subscriber.entitlements[entitlement] = {
-      "purchase_date": "2006-03-04T01:01:01Z",
-      "original_purchase_date": "2006-03-04T01:01:01Z",
+      "purchase_date": "2006-04-03T01:01:01Z",
+      "original_purchase_date": "2006-04-03T01:01:01Z",
       "expires_date": "9692-01-01T01:01:01Z",
       "is_sandbox" : false,
       "ownership_type": "PURCHASED",
@@ -53,8 +52,8 @@ for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapp
     // Add product identifier to subscriptions
     jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
       "expires_date": "9692-01-01T01:01:01Z",
-      "original_purchase_date": "2006-03-04T01:01:01Z",
-      "purchase_date": "2006-03-04T01:01:01Z",
+      "original_purchase_date": "2006-04-03T01:01:01Z",
+      "purchase_date": "2006-04-03T01:01:01Z",
       "is_sandbox" : false,
       "ownership_type": "PURCHASED",
       "store": "app_store"
